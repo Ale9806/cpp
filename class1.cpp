d41a62d5e91fcc90b907018ada6e0aafa3753693
tacos <sagrario3098@gmail.com>
2022-02-04 17:59:52 -0600

    hello

---
+++ class1.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main()  // Main function
+{
+    std::cout << "Hello World\n"; // Print  some info
+    return 0;                    // c++ has to return 0 to avoid error 
+}
